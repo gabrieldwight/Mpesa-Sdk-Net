3bb5a8fc996c03c14e58683af5ac46b2a04676d7
Gabriel Odero <gabupostiga.gdco@gmail.com>
2020-12-19 22:14:05 +0300

    Asp net core sample added

---
+++ Samples/MpesaSdk.Web/ViewModels/LipaNaMpesaOnlineViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MpesaSdk.Web.ViewModels
+{
+    public class LipaNaMpesaOnlineViewModel
+    {
+        public string Amount { get; set; }
+        public string AccountReference { get; set; }
+        public string TransactionDesc { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
