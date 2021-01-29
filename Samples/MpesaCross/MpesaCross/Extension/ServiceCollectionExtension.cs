e1f5b0e20b0aca1731a5e21f6f25a52302b65d9c
Gabriel Odero <gabupostiga.gdco@gmail.com>
2021-01-29 16:22:07 +0300

    Native http client handler extension

---
+++ Samples/MpesaCross/MpesaCross/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MpesaSdk.Extensions;
+using System.Net.Http;
+
+namespace MpesaCross.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddNativeHandlerMpesaService<THandler>(this IServiceCollection services) where THandler : HttpMessageHandler
+        {
+            services.AddMpesaService<THandler>();
+        }
+    }
+}
