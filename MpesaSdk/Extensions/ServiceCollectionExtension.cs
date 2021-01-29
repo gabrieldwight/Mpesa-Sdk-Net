9c58bd566fb4c656300c2babae1626e0cf333216
Gabriel Odero <gabupostiga.gdco@gmail.com>
2021-01-29 17:16:45 +0300

    Added support for native http messagehandler

--- MpesaSdk/Extensions/ServiceCollectionExtension.cs
+++ MpesaSdk/Extensions/ServiceCollectionExtension.cs
@@ -47,5 +47,35 @@ namespace MpesaSdk.Extensions
                 return handler;
             }).AddPolicyHandler(request => request.Method.Equals(HttpMethod.Get) ? retryPolicy : noOpPolicy);
         }
+
+        /// <summary>
+        /// Creating a mpesa service collection with http handler to be used in projects that support dependency injection.
+        /// </summary>
+        /// Type parameter THandler to pass native implementations of HttpmessageHandler
+        /// <param name="services"></param>
+        public static void AddMpesaService<THandler>(this IServiceCollection services) where THandler : HttpMessageHandler
+        {
+            Random jitterer = new Random();
+
+            var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError()
+                .WaitAndRetryAsync(1, retryAttempt =>
+                {
+                    return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 100));
+                });
+
+            var noOpPolicy = Policy.NoOpAsync().AsAsyncPolicy<HttpResponseMessage>();
+
+            services.AddHttpClient<IMpesaClient, MpesaClient>(options =>
+            {
+#if DEBUG
+                options.BaseAddress = MpesaRequestEndpoint.SandboxBaseAddress;
+                options.Timeout = TimeSpan.FromMinutes(10);
+#else
+                options.BaseAddress = MpesaRequestEndpoint.LiveBaseAddress;
+                options.Timeout = TimeSpan.FromMinutes(10);
+#endif
+            }).ConfigurePrimaryHttpMessageHandler<THandler>()
+            .AddPolicyHandler(request => request.Method.Equals(HttpMethod.Get) ? retryPolicy : noOpPolicy);
+        }
     }
 }
