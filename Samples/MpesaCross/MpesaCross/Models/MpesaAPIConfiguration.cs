8e5ce63823dc33cb1eaade10f26ed496a255c432
Gabriel Odero <gabupostiga.gdco@gmail.com>
2021-02-28 23:38:14 +0300

    Small updates

--- Samples/MpesaCross/MpesaCross/Models/MpesaAPIConfiguration.cs
+++ Samples/MpesaCross/MpesaCross/Models/MpesaAPIConfiguration.cs
@@ -3,13 +3,13 @@
     // Note: It is better to use a web or cloud server to store all this credentials in a key vault.
     public class MpesaAPIConfiguration
     {
-        public string ConsumerKey { get; set; } = "N2PCnO12dV9eZMJXafBPOX8mGYDY5dbq";
-        public string ConsumerSecret { get; set; } = "gZhFpwuON2MxiQ6Z";
-        public string PassKey { get; set; } = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+        public string ConsumerKey { get; set; } = "{Your test/production consumerkey}";
+        public string ConsumerSecret { get; set; } = "{Your test/production consumerSecret}";
+        public string PassKey { get; set; } = "{Your test/production Lipa Na Mpesa Online PassKey}";
         public string ConfirmationUrl { get; set; } = "{Your test/production confirmationurl for receiving C2B payments}";
         public string ValidationUrl { get; set; } = "{Your test/production validationurl for receiving C2B payments}";
-        public string CallBackUrl { get; set; } = "https://mpesasdk-web.conveyor.cloud/api/PaymentCallback/{requestId}/StkPushCallback";
-        public string LNMOshortCode { get; set; } = "174379";
+        public string CallBackUrl { get; set; } = "{Your test/production callbackurl for receiving STK Push payments}";
+        public string LNMOshortCode { get; set; } = "{Your test/production Lipa Na Mpesa Online ShortCode}";
         public string C2BShortCode { get; set; } = "{Your test/production C2BShortCode}";
         public string C2BMSISDNTest { get; set; } = "{Your test/production C2BMSISDN}";
     }
