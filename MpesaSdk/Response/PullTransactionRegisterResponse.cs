0b3518eed1d3762bd9e94399926b83a2162a8fab
Gabriel Odero <gabupostiga.gdco@gmail.com>
2021-05-03 13:39:07 +0300

    Pull transaction register response added

---
+++ MpesaSdk/Response/PullTransactionRegisterResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace MpesaSdk.Response
+{
+    public class PullTransactionRegisterResponse
+    {
+        [JsonProperty("ResponseRefID")]
+        public string ResponseRefID { get; set; }
+
+        [JsonProperty("ResponseStatus")]
+        public string ResponseStatus { get; set; }
+
+        [JsonProperty("ShortCode")]
+        public string ShortCode { get; set; }
+
+        [JsonProperty("ResponseDescription")]
+        public string ResponseDescription { get; set; }
+    }
+}
