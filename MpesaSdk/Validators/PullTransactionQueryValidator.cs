2b73f64fdc8b5cba9678b7fa09caf048f1410574
Gabriel Odero <gabupostiga.gdco@gmail.com>
2021-05-03 13:35:01 +0300

    Pull transaction query validator added

---
+++ MpesaSdk/Validators/PullTransactionQueryValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MpesaSdk.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MpesaSdk.Validators
+{
+    public class PullTransactionQueryValidator : AbstractValidator<PullTransactionQuery>
+    {
+        public PullTransactionQueryValidator()
+        {
+            RuleFor(x => x.ShortCode)
+               .NotNull()
+               .WithMessage("{PropertyName} - The paybill or till number should not be empty.")
+               .Must(x => int.TryParse(x, out int value))
+               .WithMessage("{PropertyName} - The paybill or till number must be a numeric value.")
+               .Length(5, 7)
+               .WithMessage("{PropertyName} - The paybill or till number should be 5 to 7 account number.");
+
+            RuleFor(x => x.StartDate)
+                .NotNull()
+                .WithMessage("{PropertyName} - The start date should not be empty.");
+
+            RuleFor(x => x.EndDate)
+                .NotNull()
+                .WithMessage("{PropertyName} - The end date should not be empty.");
+
+            RuleFor(x => x.OffSetValue)
+                .NotNull()
+                .WithMessage("{PropertyName} - The offset value should not be empty.");
+        }
+    }
+}
