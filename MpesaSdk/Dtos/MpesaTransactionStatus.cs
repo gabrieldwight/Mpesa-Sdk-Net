f739a9cd95852f0c2332f888c3eba56af2b19b2b
Gabriel Odero <gabupostiga.gdco@gmail.com>
2023-08-05 16:09:39 +0300

    Added Originator ConversationID property

--- MpesaSdk/Dtos/MpesaTransactionStatus.cs
+++ MpesaSdk/Dtos/MpesaTransactionStatus.cs
@@ -35,6 +35,12 @@ namespace MpesaSdk.Dtos
         public string TransactionID { get; private set; }
 
         /// <summary>
+        /// This is a global unique identifier for the transaction request returned by the API proxy upon successful request submission. If you don’t have the M-PESA transaction ID you can use this to query.
+        /// </summary>
+        [JsonProperty("OriginatorConversationID")]
+        public string OriginatorConversationID { get; private set; }
+
+        /// <summary>
         /// Organization/MSISDN receiving the transaction
         /// </summary>
         [JsonProperty("PartyA")]
@@ -104,6 +110,47 @@ namespace MpesaSdk.Dtos
             SecurityCredential = securityCredential;
             CommandID = Transaction_Type.TransactionStatusQuery;
             TransactionID = transactionId;
+            PartyA = partyA;
+            IdentifierType = identifierType;
+            Remarks = remarks;
+            QueueTimeOutURL = queueTimeoutUrl;
+            ResultURL = resultUrl;
+            Occasion = occasion;
+        }
+
+        /// <summary>
+        /// Mpesa Transaction Status Query data transfer object
+        /// </summary>
+        /// <param name="initiator">
+        /// The name of Initiator to initiating  the request.
+        /// This is the credential/username used to authenticate the transaction request.
+        /// </param>
+        /// <param name="securityCredential">
+        /// Encrypted password for the initiator to authenticate the transaction request.
+        /// Use <c>Credentials.EncryptPassword</c> method available under MpesaLib.Helpers to encrypt the password.
+        /// </param>
+        /// <param name="transactionId">Unique identifier to identify a transaction on M-Pesa. e.g LKXXXX1234</param>
+        /// <param name="originatorConversationID">Unique identifier to identify without transaction ID. e.g AG_XXXXX_XXX</param>
+        /// <param name="partyA">Organization/MSISDN receiving the transaction</param>
+        /// <param name="identifierType">
+        /// Type of organization receiving the transaction
+        /// 1 – MSISDN
+        /// 2 – Till Number
+        /// 4 – Organization short code
+        /// </param>
+        /// <param name="remarks">
+        /// Comments that are sent along with the transaction
+        /// </param>
+        /// <param name="queueTimeoutUrl">The path that stores information of time out transaction. https://ip or domain:port/path</param>
+        /// <param name="resultUrl">The path that stores information of transaction. https://ip or domain:port/path</param>
+        /// <param name="occasion">Optional Parameter. (upto 100 characters)</param>
+        public MpesaTransactionStatus(string initiator, string securityCredential, string transactionId, string originatorConversationID, string partyA, string identifierType, string remarks, string queueTimeoutUrl, string resultUrl, string occasion)
+        {
+            Initiator = initiator;
+            SecurityCredential = securityCredential;
+            CommandID = Transaction_Type.TransactionStatusQuery;
+            TransactionID = transactionId;
+            OriginatorConversationID = originatorConversationID;
             PartyA = partyA;
             IdentifierType = identifierType;
             Remarks = remarks;
